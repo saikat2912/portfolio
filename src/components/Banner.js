a0a7171c6d40019026298379eae513bd1006ce46
saikat2912 <saikat2270@gmail.com>
2024-04-01 19:19:54 +0530

    Banner code

---
+++ src/components/Banner.js
@@ -0,0 +1,69 @@
+import { useState,useEffect } from "react";
+import { Col, Container,Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import ImageHeader from '../assets/img/header-img.svg';
+
+export const Banner =()=>{
+
+    const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+
+
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline"></span>
+                        <h1>{`Hi I am webcoded`}<span className="wrap">{text}</span></h1>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                        <button onClick={()=>console.log('connect')}>Lets Connect<ArrowRightCircle/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={ImageHeader} alt="header img"/>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
